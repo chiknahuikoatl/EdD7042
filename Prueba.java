8594b73aa1619d6e4cf9a960fe292ed71eda5df8
Angel Castro <castro_angel@ciencias.unam.mx>
2023-01-31 22:28:32 -0600

    Angel Castro Reyes

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba {
 	public static void main(String[] args){
 		System.out.println("Hola mundo");
 		System.out.println("ChiknahuiKoatl");
+		System.out.println("Angeluskings04");
 	}
 
 }
