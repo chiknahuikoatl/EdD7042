209688538eeb9f39848f39eea5d49b4bbcc6a4fa
LuyiAngel <luyiangel@ciencias.unam.mx>
2023-02-02 11:46:26 -0600

    Luis Angel Santiago Cruz

--- Prueba.java
+++ Prueba.java
@@ -25,7 +25,7 @@ public class Prueba {
 		System.out.println("JoseAntonio9");
 		System.out.println("cr1stan");
 		System.out.println("allanjh10");
-		System.out.println("LuyiAngel");
+		System.out.print("LuyiAngel");
 	}
 
 }
